fecb3b3caf52c7757e5206323d5641824aa842ab
cat271 <cristianalexis271@gmail.com>
2024-02-28 22:15:12 -0700

    Creacion pagina MeSI

---
+++ indexzzz.php
@@ -0,0 +1,114 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<!--<meta name="viewport" content="user-scalable=no,initial-scale=1,maximum-scale=1"/>-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Mecatr&oacute;nica y Suministros Industriales S.A. de C.V - MeSI</title>
+	<link rel="shortcut icon" href="images/LogoEngr.png" type="image/x-icon">
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+</head>
+<body>
+	
+	<header>
+		<span class="nav-bar" id="btnMenu"><i class="fas fa-bars"></i></span>
+		<nav class="main-nav">
+			<ul class="menu" id="menu">
+				<li class="menu__item container-submenu"><a href="#" class="menu__link submenu-btn">Productos</a>
+					<ul class="submenu">
+						<li class="menu__item"><a href="https://mesi.mx/Delta.html" class="menu__link">Delta</a></li>
+						<li class="menu__item"><a href="https://mesi.mx/htm-sensors.html" class="menu__link">HTM Sensors</a></li>
+						<li class="menu__item"><a href="" class="menu__link">Optex Fa</a></li>
+						<li class="menu__item"><a href="" class="menu__link">Igus</a></li>
+						<li class="menu__item"><a href="" class="menu__link">Weg</a></li>
+						<li class="menu__item"><a href="" class="menu__link">STC</a></li>
+						<li class="menu__item"><a href="" class="menu__link">Fanuc Robotics</a></li>
+						<li class="menu__item"><a href="" class="menu__link">Pro-face</a></li>
+						<li class="menu__item"><a href="" class="menu__link">Fagor Automation</a></li>
+					</ul>
+				</li>
+
+				<li class="menu__item container-submenu"><a href="#" class="menu__link submenu-btn">Divisiones</a>
+					<ul class="submenu">
+						<li class="menu__item"><a href="" class="menu__link">Automatizaci&oacute;n</a></li>
+						<li class="menu__item"><a href="" class="menu__link">Distribuci&oacute;n</a></li>
+						<li class="menu__item"><a href="" class="menu__link">Maquinados</a></li>
+						<li class="menu__item"><a href="" class="menu__link">Promociones</a></li>
+						<li class="menu__item"><a href="" class="menu__link">Boletines</a></li>
+					</ul>
+				</li>
+
+				<li class="menu__item"><a href="" class="menu__link">Entrenamientos</a></li>
+
+				<li class="menu__item container-submenu texor"><a href="#" class="menu__link submenu-btn">Nosotros</a>
+					<ul class="submenu">
+						<li class="menu__item"><a href="" class="menu__link">&iquest;Qui&eacute;nes somos?</a></li>
+						<li class="menu__item"><a href="" class="menu__link">Misi&oacute;n y Visi&oacute;n</a></li>
+						<li class="menu__item"><a href="" class="menu__link">Valores</a></li>
+						<li class="menu__item"><a href="" class="menu__link">Cont&aacute;ctanos</a></li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+		<script src="js/menu.js"></script>
+	</header>
+	<section class="contenido__cuerpo">
+		
+		<div class="GaleriaPortada">
+			<center>
+			<h1>Bienvenidos</h1>
+			<br>
+			<img src="images/logo.png">
+			<br>
+			<p>Expertos en &aacute;reas de PLCs, Drivers, Motion, Neumática, hidráulica y Sensores.</p>
+		</center>
+		</div>
+
+		<section class="ElgProd">
+			<center><br><h1>Elige tu producto:</h1></center><br>
+			<section class="EligeProd">
+				<div class="in-flex"><center><img src="http://www.mercado-ideal.com/fotos/fotosh/HTM-SENSORS.jpg" width="100%"></center></div>
+				<!--<div class="in-flex">En MeSI, contamos con los mejores productos para las &aacute;reas de PLCs, Drivers, Motion, Neum&aacute;tica, Hidr&aacute;ulica y Sensores. En MeSI, contamos con los mejores productos para las &aacute;reas de PLCs, Drivers, Motion, Neum&aacute;tica, Hidr&aacute;ulica y Sensores.</div>-->
+				<div class="in-flex">
+					<div><h2>Elige el producto que m&aacute;s se adapte a tus necesidades</h2></div>
+					<div>En MeSI, contamos con los mejores productos para las &aacute;reas de PLCs, Drivers, Motion, Neum&aacute;tica, Hidr&aacute;ulica y Sensores.</div>
+				</div>
+			</section>
+		</section>
+
+		<div class="ElgMarc">
+			<center><br><h1>Elige una marca</h1><br><br></center>
+			<center><p><a href="http://www.deltaww.com/" target="_blank"><img src="images/DeltaLogo.png"></a> <a href="http://www.htmsensors.com/" target="_blank"><img src="images/HTMLogo.png"></a> <a href="http://www.optex-fa.com/" target="_blank"><img src="images/OptexLogo.png"></a> <a href="http://www.igus.com/default.asp?c=us&L=en" target="_blank"><img src="images/IGUSLogo.png"></a> <a href="http://www.weg.net/mx" target="_blank"><img src="images/WEGLogo.png"></a> <a href="http://www.stcvalve.com/" target="_blank"><img src="images/STCLogo.png"></a> <a href="https://www.fagorautomation.com/" target="_blank"><img src="images/FAGORLogo.png"></a> <a href="https://www.proface.com/en" target="_blank"><img src="images/PROFACELogo.png"></a> <a href="https://www.gavazzionline.com/CGNA/Home" target="_blank"><img src="images/CGLogo.png"></a> <a href="http://www.fanucrobotics.com.mx/Default.aspx" target="_blank"><img src="images/FANUCLogo.png"></a></p><br><br></center>
+		</div>
+
+		<div class="PruebaGaleria">
+
+		</div>
+		
+		<div class="FinalPag">
+			<center><br><br>
+			<h1><i>En MeSI, buscamos y ofrecemos soluci&oacute;n a los problemas</i></h1>
+			<br><br>
+			<p>Ciudad Ju&aacute;rez, M&eacute;xico</p>
+			<p>Tel&eacute;fonos: (656) 325 8545 - (656) 325 8898</p>
+			<p>Estados Unidos: (915) 239 3697</p>
+			<br>
+			<img src="https://cdn-icons-png.flaticon.com/512/174/174879.png" width="50px">
+			<p>WhatsApp</p>
+			<p><a href="https://wa.me/526563194898" target="_blank">(656) 319 4898</a> - <a href="https://wa.me/526563600152" target="_blank">(656) 360 0152</a>
+			<p>Correo: <a href="mailto:ventas@mesi.mx" target="_blank">ventas@mesi.mx</p></a>
+			<br><br>
+			<p>Mecatr&oacute;nica y Suministros Industriales S.A. de C.V.</p>
+			<p>©Derechos Reservados MMXXI</p>
+			<br></center>
+		</div>
+		<div class="logoEsq">
+			<a href=""><img src="images/logoFonBlan.png" width="123" height="53"></a>
+		</div>
+		<div class="logoEsqMov">
+			<a href=""><img src="images/logoFonBlan.png" width="145" height="63"></a>
+		</div>
+	</section>
+</body>
+</html>
